e2eb15e46c93948672eda302d0c415eb1b822fbc
Jose A. Pinell <japinell@yahoo.com>
2025-08-31 22:38:56 -0700

    Removed unused imports in GroceryList widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:flutter_shopping_list/data/categories.dart";
-import "package:flutter_shopping_list/data/dummy_items.dart";
 import "package:http/http.dart" as http;
 import "dart:convert";
 import "package:flutter_dotenv/flutter_dotenv.dart";
