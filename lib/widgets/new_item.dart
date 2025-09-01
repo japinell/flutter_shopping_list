44ad491a70bd36e5ed5deee2c314701c159acdf2
Jose A. Pinell <japinell@yahoo.com>
2025-08-31 17:32:27 -0700

    Fixed display issue in NewItem widget

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_shopping_list/data/categories.dart";
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -16,7 +17,53 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(title: const Text("Add New Item")),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Text("The form..."),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text("Name")),
+                validator: (value) {
+                  return "Please enter a name";
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(label: Text("Quantity")),
+                      initialValue: "1",
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.name),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
